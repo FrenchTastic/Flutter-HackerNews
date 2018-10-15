9ff023795ba79c892a0d8069a72df3e6b030b72c
Romain Pardon <romain.pardon@almerys.com>
2018-10-15 16:48:51 +0200

    Bloc bloc implemented, start of comment display

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
@@ -24,7 +27,9 @@ Route routes (RouteSettings settings) {
   } else {
     return MaterialPageRoute(
       builder: (context) {
-        int itemId = int.parse(settings.name.replaceFirst('/', ''));
+        final commentsBloc = CommentsProvider.of(context);
+        final itemId = int.parse(settings.name.replaceFirst('/', ''));
+        commentsBloc.fetchItemWithComments(itemId);
         return NewsDetail(itemId: itemId);
       }
     );
